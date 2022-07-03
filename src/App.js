4d9493a1ea96fb39ad98f6e8addf9859533e6d32
Ahmad Ibrahim <ahmadaburabee3@gmail.com>
2022-07-03 14:54:59 -0600

    refactored: moved code to UnorderList component
    - at first I had duplibated code in app.js and unorderlist.js. So i refactored the code so that unorderlist handled all the functionality

--- src/App.js
+++ src/App.js
@@ -1,32 +1,22 @@
-import { useState } from "react";
 import UnorderedList from "./components/UnorderList";
 import data from "./challenge-data.json";
-
 import "./css/global.css";
 
 function App() {
-  const [display, setDisplay] = useState(false);
   console.log(data);
-  const handleOpenDropDown = () => {
-    console.log("handler");
-    setDisplay(true);
-  };
+
   return data && typeof data === "object" ? (
     Object.keys(data).map((property, index) => {
       const node = data[property];
       return (
-        <ul key={index}>
-          <button onClick={() => handleOpenDropDown()}>{">"}</button>
-          <span>{"{ "}</span>
-          <label>{property} :</label>
-          <UnorderedList
-            display={display}
-            property={property}
-            node={node}
-            level={1}
-          />
-          <span>{" }"}</span>
-        </ul>
+        <UnorderedList
+          key={index}
+          group={index}
+          display={true}
+          property={property}
+          node={node}
+          level={1}
+        />
       );
     })
   ) : (
