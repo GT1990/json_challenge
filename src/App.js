bf96399b875dcc95c9881ade1ec0b6c72d25fc2b
Ahmad Ibrahim <ahmadaburabee3@gmail.com>
2022-07-03 00:52:20 -0600

    import: UnorderList component
    - imported unorderlist compoenent
    
    - returned an array that iterates through all the keys in the json data object passing the data to the unorderlist component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
-import data from "./test-data.json";
+import { useState } from "react";
+import UnorderedList from "./components/UnorderList";
+import data from "./challenge-data.json";
+
+import "./css/global.css";
 
 function App() {
-  console.log("Data: ", data);
-  return <main className="main"></main>;
+  const [display, setDisplay] = useState(false);
+  console.log(data);
+  const handleOpenDropDown = () => {
+    console.log("handler");
+    setDisplay(true);
+  };
+  return data && typeof data === "object" ? (
+    Object.keys(data).map((property, index) => {
+      const node = data[property];
+      return (
+        <ul key={index}>
+          <button onClick={() => handleOpenDropDown()}>></button>
+          <span>{"{ "}</span>
+          <label>{property} :</label>
+          <UnorderedList
+            display={display}
+            property={property}
+            node={node}
+            level={1}
+          />
+          <span>{" }"}</span>
+        </ul>
+      );
+    })
+  ) : (
+    <p>The data needs to be an object</p>
+  );
 }
-
 export default App;
