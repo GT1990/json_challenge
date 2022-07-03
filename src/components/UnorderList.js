a649bad731009fe816cda573d69334d194ac4824
Ahmad Ibrahim <ahmadaburabee3@gmail.com>
2022-07-03 14:56:47 -0600

    refactored: moved all duplicated code from app.js
    - app.js had duplicated code that was the same in unorderlist component. So i deleteded the duplicated code and handled everything in the unorderedlist component

--- src/components/UnorderList.js
+++ src/components/UnorderList.js
@@ -1,22 +1,62 @@
+import { useState } from "react";
 import ListItem from "./ListItem";
 
-function UL({ display, property, node, level }) {
+import handleDropDownToggle from "./handleDropDownToggle";
+
+function UnorderedList({ group, display, property, node, level }) {
+  const [displayState, setDisplayState] = useState([]);
   return (
     <>
-      {node && typeof node === "object" ? (
-        Object.keys(node).map((property, index) => {
-          return (
-            <ul key={`${index}${level}`} className={display ? null : `hidden`}>
-              {typeof node[property] === "object" ? `${property} :` : null}
-              <UL property={property} node={node[property]} level={++level} />
-            </ul>
-          );
-        })
+      {node &&
+      typeof node === "object" &&
+      !(Array.isArray(node) && node.length === 0) ? (
+        <ul
+          key={`${group}${level}`}
+          className={display ? group : `hidden ${group}`}
+        >
+          <button
+            className={displayState.includes(group) ? `rotate ${group}` : group}
+            onClick={() => handleDropDownToggle(group, setDisplayState)}
+          >
+            {">"}
+          </button>
+          <label className={group}>{property} :</label>
+          {Object.keys(node).map((property, index) => {
+            return (
+              <UnorderedList
+                key={`${index}${level}`}
+                group={group + 1}
+                display={displayState.includes(group) ? true : false}
+                property={property}
+                node={node[property]}
+                level={++level}
+              />
+            );
+          })}
+        </ul>
       ) : (
-        <ListItem property={property} value={node} level={++level} />
+        <>
+          {Array.isArray(node) && node.length === 0 ? (
+            <ListItem
+              group={group}
+              display={display ? true : false}
+              property={property}
+              value={"[ ]"}
+              level={++level}
+            />
+          ) : (
+            <ListItem
+              group={group}
+              display={display ? true : false}
+              property={property}
+              value={node}
+              level={++level}
+            />
+          )}
+        </>
       )}
     </>
   );
 }
 
-export default UL;
+export default UnorderedList;
